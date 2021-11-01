13bb9cc42e862a03eb5a0eb4169c4dbe6357f72b
nevesfelipe47 <santosfelpe47@gmail.com>
2021-11-01 13:26:28 -0300

    first commit

---
+++ Curso Aluno4.sql
@@ -0,0 +1,18 @@
+--------AVG Retrona a média dos valores em um grupo --- os nulos são ignorados---
+
+select * from senso; 
+
+
+SELECT AVG(ID_PACIENTE) AS MÉDIA FROM PACIENTES
+WHERE DT_ENTRADA='2021';
+
+
+select estado,avg(populacao) from senso
+where ano='2014'
+group by estado
+order by 2 desc;
+
+select estado,avg(populacao) from senso
+where ano='2013'
+group by estado
+order by 2 desc;
