ef69f9f17e7adaccc5800d0be01b05370256988e
nevesfelipe47 <santosfelpe47@gmail.com>
2021-11-03 16:29:22 -0300

    atualizacao add arquivo cursoaluno 6 CAPITULO 6

--- Curso aluno cap6.sql
+++ Curso aluno cap6.sql
@@ -3,10 +3,15 @@
 
   SELECT Count(*) AS QTDE_REGISTROS,
           Avg(TOTAL) AS MEDIA,
-          Avg(TOTAL) AS MEDIA,
+          Round (Avg(TOTAL),2) AS MEDIA, --- Round -> arrendondar
           Max(TOTAL) AS MAXIMO,
           Min(TOTAL) AS MINIMO,
           Sum(TOTAL) AS SOMA,
           Max (DATA) AS DATA_MAIOR,
           Min (DATA) AS DATA_MENOR
           FROM TCONTRATO;
+
+
+          ---- PROXIMO CODIGO SEQUENCIAL TABELA
+          SELECT Max(COD_CONTRATO) + 1 AS proximo_codigo
+          FROM tcontrato;
